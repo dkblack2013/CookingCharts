f88d54f2bb8d9a3f5f7982e35d3efce0fdea7979
David Black <65377792+dkblack2013@users.noreply.github.com>
2020-12-10 07:58:45 -0500

    Updated app to show two Gantt Charts at once.
    Other minor details updated as well.

--- Cooking_Timeline_App_v3.R
+++ Cooking_Timeline_App_v3.R
@@ -5,11 +5,11 @@ library(tidyr)
 library(ggplot2)
 library(plyr)
 library(dplyr)
-
+#library(gridExtra)
 
 # --- Input datafile
 
-AllData <- data.frame(Project = c("Mac&Cheese", "Mac&Cheese", "Mac&Cheese", "Mac&Cheese", "Mac&Cheese", "Mac&Cheese",
+AllData <- data.frame(Recipe = c("Mac&Cheese", "Mac&Cheese", "Mac&Cheese", "Mac&Cheese", "Mac&Cheese", "Mac&Cheese",
                                   "Green Beans","Green Beans","Green Beans","Green Beans"),  
                       task = c("Boil 6 cups water - 5 min", "Stir in pasta - 1 min",
                                "Cook, stirring occationally - 8 min",
@@ -25,6 +25,8 @@ AllData <- data.frame(Project = c("Mac&Cheese", "Mac&Cheese", "Mac&Cheese", "Mac
                               "first_1_side", "first_2_side", "first_3_side", "first_4_side"),
                       start = c("tm1","after first_1", "after first_2", "after first_3", "after first_4", "after first_5",
                                 "tm1","after first_1_side", "after first_2_side", "after first_3_side"),
+                      # end = c("5d", "1d", "8d", "1d", "5d", "1d",
+                      #         "4d", "7d", "2d", "1d"),
                       end = c("5m", "1m", "8m", "1m", "5m", "1m",
                               "4m", "7m", "2m", "1m"),
                       stringsAsFactors = FALSE)
@@ -37,14 +39,15 @@ ui <- fluidPage(
   titlePanel("Cooking Timeline Application"),
   
   sidebarLayout(
-    sidebarPanel(                       # --- setup LHS data input frames ---
+    sidebarPanel(
       
+      selectInput("Proj", "Meal",
+                  c(unique(as.character(AllData$Recipe))
+                    )),
       
-      selectInput("Proj", "Project",
-                  c(unique(as.character(AllData$Project)),
-                    unique(as.character(df$Project)),
-                    unique(as.character(df2$Project)))),
-      
+      selectInput("Proj2", "Meal 2",
+                  c(unique(as.character(AllData$Recipe))
+                    )),
       
       selectInput("Stg", "Stage",
                   c("All", unique(as.character(AllData$task)))),
@@ -54,27 +57,33 @@ ui <- fluidPage(
     mainPanel(
       
       tabsetPanel(type = "tabs",
-                  tabPanel("Gantt Chart", DiagrammeROutput("plot")),
-                  tabPanel("Data Table", tableOutput("table"))))
+                  tabPanel("Gantt Chart", 
+                                  DiagrammeROutput("plot"), height="50%",width="100%",
+                                  DiagrammeROutput("plot2"), height="50%", width="100%"
+                  ),
+                  tabPanel("Data Table", 
+                           tableOutput("table"),
+                           tableOutput("table2"))))
     
   )
 )
 
 server <- function(input, output) {
   
-  # --- filter the selected project into a reactive function (access later using () suffix) ---
-  SelectedProject <- reactive({dplyr::filter(AllData, Project == input$Proj)})
+  # --- filter the selected Recipe into a reactive function (access later using () suffix) ---
+  SelectedRecipe <- reactive({dplyr::filter(AllData, Recipe == input$Proj)})
+  SelectedRecipe2 <- reactive({dplyr::filter(AllData, Recipe == input$Proj2)})
   
   output$plot <- renderDiagrammeR({
     mermaid(
       paste0(
         "gantt", "\n", 
         "dateFormat  YYYY-MM-DD", "\n", 
-        "title Gantt Chart - Project ", input$Proj, "\n",
-        
+        #"axisFormat %M", "\n",
+        "title Meal Instructions - ", input$Proj, "\n",
         # --- unite the first two columns (task & status) and separate them with ":" ---
         # --- then, unite the other columns and separate them with "," ---
-        paste(SelectedProject() %>%
+        paste(SelectedRecipe() %>%
                 unite(i, task, status, sep = ":") %>%
                 unite(j, i, pos, start, end, sep = ",") %>%
                 .$j, 
@@ -84,10 +93,29 @@ server <- function(input, output) {
     )
   })
   
-  output$table <- renderTable({SelectedProject()})   
+  output$plot2 <- renderDiagrammeR({
+    mermaid(
+      paste0(
+        "gantt", "\n", 
+        "dateFormat  YYYY-MM-DD", "\n", 
+        #"axisFormat %M", "\n",
+        "title Meal Instructions - ", input$Proj2, "\n",
+        # --- unite the first two columns (task & status) and separate them with ":" ---
+        # --- then, unite the other columns and separate them with "," ---
+        paste(SelectedRecipe2() %>%
+                unite(i, task, status, sep = ":") %>%
+                unite(j, i, pos, start, end, sep = ",") %>%
+                .$j, 
+              collapse = "\n"
+        ), "\n"
+      )
+    )
+  })
+  
+  output$table <- renderTable({SelectedRecipe()}) 
+  output$table2 <- renderTable({SelectedRecipe2()})
   
 }       
-
 
 # --- run application ---
 shinyApp(ui = ui, server = server)
